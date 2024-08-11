2a41127f66c10312418b695804574e54175af990
meghal kc <67979946+Meghal-kc@users.noreply.github.com>
2024-08-11 01:28:44 -0700

    Add files via upload

---
+++ installer.bat
@@ -0,0 +1,71 @@
+Windows Registry Editor Version 5.00
+
+[HKEY_CURRENT_USER\Control Panel\Mouse]
+"MouseSensitivity"="8"
+"SmoothMouseXCurve"=hex:9e,00,fa,00,a4,00,a3,00,ac,00,8f,00,a0,00,99,00,99,00,\
+  b1,00,89,00,f1,00,81,00,8c,00,8d,00,fd,00,85,00,a2,00,89,00,ff,00,8b,00,a4,\
+  00,ac,00,b8,00,92,00,80,00,9a,00,a7,00,9e,00,a3,00,f8,00,af,00,98,00,9b,00,\
+  89,00,b0,00,87,00,9c,00,81,00,bf,00,87,00,bf,00,b8,00,9c,00,92,00,9f,00,fd,\
+  00,b2,00,a9,00,90,00,9a,00,b8,00,ac,00,a5,00,a4,00,f8,00,ad,00,9b,00,89,00,\
+  f1,00,81,00,8c,00,99,00,ff,00,8b,00,a3,00,92,00,be,00,ac,00,a1,00,fd,00,9f,\
+  00,91,00,9f,00,b0,00,f9,00,92,00,9b,00,89,00,f1,00,81,00,8c,00,8d,00,fb,00,\
+  85,00,8c,00,bb,00,f5,00
+"SmoothMouseYCurve"=hex:9b,00,8f,00,9e,00,b8,00,92,00,fa,00,a0,00,f8,00,9d,00,\
+  8d,00,85,00,af,00,98,00,9b,00,89,00,b0,00,85,00,8c,00,af,00,bf,00,87,00,bf,\
+  00,b8,00,81,00,92,00,9f,00,a4,00,a6,00,a9,00,80,00,9a,00,9f,00,9c,00,9b,00,\
+  89,00,f1,00,81,00,8c,00,8d,00,bf,00,87,00,8c,00,89,00,ff,00,8b,00,a4,00,86,\
+  00,a4,00,aa,00,a6,00,86,00,b8,00,ac,00,8f,00,a4,00,fa,00,a9,00,90,00,9a,00,\
+  fd,00,81,00,8c,00,f8,00,af,00,85,00,b2,00,bb,00,83,00,9a,00,a5,00,f1,00,fa,\
+  00,84,00,a4,00,92,00,a0,00,aa,00,80,00,9e,00,a4,00,81,00,8c,00,f8,00,af,00,\
+  85,00,9c,00,8d,00,bf,00,87,00,bf,00,f5,00,f5,00
+"MouseTrails"="0"
+"MouseThreshold1"="0"
+"MouseThreshold2"="0"
+"MouseSpeed"="1"
+"ActiveWindowTracking"=dword:00000000
+"Beep"="No"
+"DoubleClickHeight"="4"
+"DoubleClickSpeed"="500"
+"DoubleClickWidth"="4"
+"ExtendedSounds"="No"
+"MouseHoverHeight"="0"
+"MouseHoverTime"="30"
+"MouseHoverWidth"="0"
+"SnapToDefaultButton"="0"
+"SwapMouseButtons"="0"
+"DoubleClickSpeed2"="0,47"
+"DoubleClickWidth2"="0,5"
+"TcpWindowSize"=dword:0005ae4c
+"TCPDelAckTicks"=dword:00000005
+"Tcp1323Opts"=dword:00000003
+"TcpMaxDataRetransmissions"=dword:00000003
+"SackOpts"=dword:00000001
+"DefaultTTL"=dword:00007fff
+"Beep2"="No"
+"EnablePMTUDiscovery"=dword:00000001
+"EnablePMTUBHDetect"=dword:00000000
+
+[HKEY_CURRENT_USER\Control Panel\Mouse\HKEY_LOCAL_MACHINE]
+
+[HKEY_CURRENT_USER\Control Panel\Mouse\HKEY_LOCAL_MACHINE\SYSTEM]
+
+[HKEY_CURRENT_USER\Control Panel\Mouse\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet]
+
+[HKEY_CURRENT_USER\Control Panel\Mouse\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services]
+
+[HKEY_CURRENT_USER\Control Panel\Mouse\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip]
+
+[HKEY_CURRENT_USER\Control Panel\Mouse\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters]
+"AEnablePMTUBHDetect"=dword:00000000
+"EnablePMTUDiscovery"=dword:00000001
+"GlobalMaxTcpWindowSize"=dword:00007fff
+"TcpMaxDupAcks"=dword:00000002
+"SackOpts"=dword:00000001
+"Tcp1323Opts"=dword:00000001
+"TcpWindowSize"=dword:00007fff
+
+[HKEY_CURRENT_USER\Control Panel\Mouse\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\Interfaces]
+"MouseSpeed"="0"
+"MouseThreshold1"="0"
+"MouseThreshold2"="0"
+
